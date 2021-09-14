bbb220695f020d32ddd8be6eace80685aef79ed9
Afnanafsal <87473921+Afnanafsal@users.noreply.github.com>
2021-09-14 07:46:53 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -118,7 +118,7 @@ async def start(bot, update):
     
     await bot.send_photo(
         chat_id=update.chat.id,
-        photo="https://telegra.ph/file/3b669404749c82ec2e02e.jpg",
+        photo="https://telegra.ph/file/722a66781e1c751314508.jpg",
         caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
         reply_markup=reply_markup,
